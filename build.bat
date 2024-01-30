e22d36f76cde28dd79e2160db8595e1a298ee954
douglasPedrogao <douglas.pedrogao@gmail.com>
2024-01-29 21:50:05 -0300

    Adding debug flags for gdb

--- build.bat
+++ build.bat
@@ -7,5 +7,5 @@ set RAYLIB_INCLUDE_DIR=C:\raylib\raylib\src
 set RAYLIB_LIB_DIR=C:\raylib\raylib\src
 set RAYLIB_RES_FILE=C:\raylib\raylib\src\raylib.rc.data
 
-gcc ../src/main.c -o main.exe %RAYLIB_RES_FILE% -s -O3 -I%RAYLIB_INCLUDE_DIR% -L%RAYLIB_LIB_DIR% -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows
+gcc ../src/main.c -g -o main.exe %RAYLIB_RES_FILE% -O3 -I%RAYLIB_INCLUDE_DIR% -L%RAYLIB_LIB_DIR% -lraylib -lopengl32 -lgdi32 -lwinmm -std=c99 -Wall -mwindows
 popd 
